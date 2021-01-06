1436fa5db19dfa2eb509c042b5f014aa3b04bb23
deeptivenugopal <51504458+deeptivenugopal@users.noreply.github.com>
2021-01-06 17:50:02 +0530

    Add files via upload

---
+++ Hangman/HangMan_Basic_FromGFG.py
@@ -0,0 +1,81 @@
+# library that we use in order to choose  
+# on random words from a list of words 
+import random
+
+# Here the user is asked to enter the name first 
+name = input("Enter your name: ")
+
+print("Good Luck! ",name)
+
+#Create list of words
+
+words = ['rainbow', 'computer', 'science', 'programming',  
+         'python', 'mathematics', 'player', 'condition',  
+         'reverse', 'water', 'board', 'geeks']
+
+# Function will choose one random 
+# word from this list of words
+word = random.choice(words)
+
+print("Guess the characters")
+
+#Guessed string
+guesses = ''
+
+# any number of turns can be used here 
+turns = 12
+
+while turns > 0:
+
+    # counts the number of times a user fails
+    failed = 0
+
+    # all characters from the input 
+    # word taking one at a time.
+    for char in word:
+
+        # comparing that character with 
+        # the character in guesses
+        if char in guesses:
+            print(char)
+        else:
+            print("_")
+            
+            # for every failure 1 will be 
+            # incremented in failure
+            failed += 1
+
+    if failed == 0:
+
+        # user will win the game if failure is 0 
+        # and 'You Win' will be given as output
+        print("You Win!!")
+
+        # this prints the correct word
+        print("The word is:",word)
+        break
+
+    print()
+    # if user has input the wrong alphabet then 
+    # it will ask user to enter another alphabet
+    guess = input("Guess a letter: ")
+
+    # every input character will be stored in guesses
+    guesses += guess
+
+    # check input with the character in word     
+    if guess not in word:
+
+        turns -= 1
+
+        # if the character doesn’t match the word 
+        # then “Wrong” will be given as output
+        print("Wrong")
+
+        # this will print the number of 
+        # turns left for the user
+        print("You have",turns,"guesses")
+
+        if turns == 0:
+            print("You Loose")
+        
